a682e0514e265da8866bea3d1201a510634a1399
Peteef <kamilptf@gmail.com>
2023-12-10 21:56:23 +0100

    Initial commit

---
+++ ShoppingBuddy/ShoppingBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingBuddyApp.swift
+//  ShoppingBuddy
+//
+//  Created by Kamil Turek on 10/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
